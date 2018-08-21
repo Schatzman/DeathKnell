c324208380f8d1c9c38a1a062774fef705c0c619
daniel-schatz-miller <daniel.schatz-miller@logrhythm.com>
2018-08-21 13:04:59 -0600

    remove rpmbuild dir

--- scripts/buildRpm.sh
+++ scripts/buildRpm.sh
@@ -19,7 +19,7 @@ fi
 
 BUILD="$1"
 
-# As version number we use the commit number on HEAD 
+# As version number we use the commit number on HEAD
 # we do not bother with other branches for now
 GIT_VERSION=`git rev-list --branches HEAD | wc -l`
 VERSION="1.$GIT_VERSION"
@@ -31,6 +31,7 @@ CWD=$PWD/$PACKAGE
 DISTDIR=$CWD/dist/$PACKAGE
 PATH=$PATH:/usr/local/probe/bin:$PATH
 
+sudo rm -rf ~/rpmbuild
 rpmdev-setuptree
 cp packaging/$PACKAGE.spec ~/rpmbuild/SPECS
 rm -f $PACKAGE-$VERSION.tar.gz
